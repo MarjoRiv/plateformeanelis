e145f65409fd82e40bcefc754f390f828e14d8f9
David Santiago <dawlys@gmail.com>
2015-01-14 20:59:14 +0100

    Help part added

--- src/Application/MainBundle/Controller/DefaultController.php
+++ src/Application/MainBundle/Controller/DefaultController.php
@@ -10,4 +10,9 @@ class DefaultController extends Controller
     {
         return $this->render('ApplicationMainBundle:Default:index.html.twig');
     }
+
+    public function aideAction()
+    {
+        return $this->render('ApplicationMainBundle:Default:aide.html.twig');
+    }
 }
