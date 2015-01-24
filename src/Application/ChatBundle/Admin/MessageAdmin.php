b3d15c10f730e631fa446a117561853d686a16bd
David Santiago <dawlys@gmail.com>
2015-01-24 13:39:48 +0100

    Administration part added

---
+++ src/Application/ChatBundle/Admin/MessageAdmin.php
@@ -0,0 +1,44 @@
+<?php
+
+namespace Application\ChatBundle\Admin;
+
+use Sonata\AdminBundle\Admin\Admin;
+use Sonata\AdminBundle\Datagrid\ListMapper;
+use Sonata\AdminBundle\Datagrid\DatagridMapper;
+use Sonata\AdminBundle\Form\FormMapper;
+
+class MessageAdmin extends Admin
+{
+    // Fields to be shown on create/edit forms
+    protected function configureFormFields(FormMapper $formMapper)
+    {
+        $formMapper
+            ->add('enabled')
+        ;
+    }
+
+    // Fields to be shown on filter forms
+    protected function configureDatagridFilters(DatagridMapper $datagridMapper)
+    {
+        $datagridMapper
+        ->add('date')
+        ;
+    }
+
+    // Fields to be shown on lists
+    protected function configureListFields(ListMapper $listMapper)
+    {
+        $listMapper
+            ->addIdentifier('user.name')
+            ->add('message')
+            ->add('enabled')
+            // add custom action links
+            ->add('_action', 'actions', array(
+                'actions' => array(
+                    'show' => array(),
+                    'edit' => array(),
+                )
+            ))
+        ;
+    }
+}
