dbb16075da1e989c6571c95d54a0cef1c54b9367
David Santiago <dawlys@gmail.com>
2015-02-15 16:35:01 +0100

    issue #36 fixed

--- src/Admin/UserBundle/Entity/User.php
+++ src/Admin/UserBundle/Entity/User.php
@@ -255,7 +255,7 @@ class User extends BaseUser
         $this->maritalName          = "";
         $this->surname              = "";
         $this->address              = "";
-        $this->isEmailValid       = true;
+        $this->isEmailValid         = true;
         $this->telephone            = "";
         $this->website              = "";
         $this->promotion            = "";
@@ -1050,4 +1050,5 @@ class User extends BaseUser
     {
         return $this->mlIsimaNews;
     }
+
 }
