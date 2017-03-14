0ba786fc19b124985e2e5b3902bdca50c1b97519
sichaizze <sichaizze@gmail.com>
2017-03-14 13:18:48 +0100

    suppression pour prod de la génération en ligne de commande entité

--- app/AppKernel.php
+++ app/AppKernel.php
@@ -37,13 +37,12 @@ class AppKernel extends Kernel
             new Application\CareerBundle\ApplicationCareerBundle(),
             new Application\YearbookBundle\ApplicationYearbookBundle(),
             new Application\OffreBundle\OffreBundle(),
-            new \Sensio\Bundle\GeneratorBundle\SensioGeneratorBundle()
         );
 
         if (in_array($this->getEnvironment(), array('dev', 'test'))) {
             $bundles[] = new Symfony\Bundle\DebugBundle\DebugBundle();
             $bundles[] = new Symfony\Bundle\WebProfilerBundle\WebProfilerBundle();
-            $bundles[] = new Sensio\Bundle\DistributionBundle\SensioDistributionBundle();
+            $bundles[] = new Sensio\Bundle\DistributionBundle\SensioDistributionBundle(); 
         }
 
         return $bundles;
