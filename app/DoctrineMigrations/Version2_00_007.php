a99004a6c03b70d86cba5e8174f462c80644bdf1
Ravel Adamony <raveladamony@outlook.fr>
2018-03-18 21:10:05 +0100

    Corrections de bugs (Mailjet)

--- app/DoctrineMigrations/Version2_00_007.php
+++ app/DoctrineMigrations/Version2_00_007.php
@@ -23,10 +23,10 @@ class Version2_00_007 extends AbstractMigration {
            $this->addSql('CREATE TABLE newsletter (id INT AUTO_INCREMENT NOT NULL, newsletter VARCHAR(255) NOT NULL, commentaire VARCHAR(255) , frequence VARCHAR(255), mailjet_id VARCHAR(255) NOT NULL DEFAULT "0", PRIMARY KEY(id)) DEFAULT CHARACTER SET utf8 COLLATE utf8_unicode_ci ENGINE = InnoDB;');
            //on remplie la table newsletter avec les 4 newsletters précédantes
            $this->addSql('INSERT INTO `newsletter` (`id`, `newsletter`, `commentaire`, `frequence`) VALUES(1,"Newsletter Anelis","Actualité sur la vie de ISIMA et de l\'Anelis, des anciens (naissance, mariage, création d\'entreprise...) et du BDE","6 par an");');
-           $this->addSql('INSERT INTO `newsletter` (`id`, `newsletter`, `commentaire`, `frequence`) VALUES(1,"mlEmployments","Recherche d\'emploi","mensuel");');
-           $this->addSql('INSERT INTO `newsletter` (`id`, `newsletter`, `commentaire`, `frequence`) VALUES(2,"mlEvents","Tous les événements en France","mensuel");');
-           $this->addSql('INSERT INTO `newsletter` (`id`, `newsletter`, `commentaire`, `frequence`) VALUES(3,"mlInformations","Information à ne pas louper","annuel");');
-           $this->addSql('INSERT INTO `newsletter` (`id`, `newsletter`, `commentaire`, `frequence`) VALUES(4,"mlIsimaNews","Tout ce qui se passe à l\'isima","tous les 5 mois");');
+           $this->addSql('INSERT INTO `newsletter` (`id`, `newsletter`, `commentaire`, `frequence`) VALUES(2,"mlEmployments","Recherche d\'emploi","mensuel");');
+           $this->addSql('INSERT INTO `newsletter` (`id`, `newsletter`, `commentaire`, `frequence`) VALUES(3,"mlEvents","Tous les événements en France","mensuel");');
+           $this->addSql('INSERT INTO `newsletter` (`id`, `newsletter`, `commentaire`, `frequence`) VALUES(4,"mlInformations","Information à ne pas louper","annuel");');
+           $this->addSql('INSERT INTO `newsletter` (`id`, `newsletter`, `commentaire`, `frequence`) VALUES(5,"mlIsimaNews","Tout ce qui se passe à l\'isima","tous les 5 mois");');
           }
           try {
           	
@@ -38,10 +38,10 @@ class Version2_00_007 extends AbstractMigration {
             $this->addSql('ALTER TABLE subscriber ADD CONSTRAINT FK_AD005B69A76ED395 FOREIGN KEY (user_id) REFERENCES User (id);');
             $this->addSql('ALTER TABLE subscriber ADD CONSTRAINT FK_AD005B6922DB1917 FOREIGN KEY (newsletter_id) REFERENCES newsletter (id);');
             //on remplie la table d'association (id user // id newsletter) por les utilisateur voullant la newsletter
-            $this->addSql('INSERT INTO `subscriber` (`user_id`, `newsletter_id`) SELECT `User`.`id`,`newsletter`.`id` FROM User INNER JOIN newsletter ON mlInformations = 1 WHERE `newsletter`.`newsletter` = "mlInformations";');
-            $this->addSql('INSERT INTO `subscriber` (`user_id`, `newsletter_id`) SELECT `User`.`id`,`newsletter`.`id` FROM User INNER JOIN newsletter ON mlEmployment = 1 WHERE `newsletter`.`newsletter` = "mlEmployments";');
-            $this->addSql('INSERT INTO `subscriber` (`user_id`, `newsletter_id`) SELECT `User`.`id`,`newsletter`.`id` FROM User INNER JOIN newsletter ON mlEvents = 1 WHERE `newsletter`.`newsletter` = "mlEvents";');
-            $this->addSql('INSERT INTO `subscriber` (`user_id`, `newsletter_id`) SELECT `User`.`id`,`newsletter`.`id` FROM User INNER JOIN newsletter ON mlIsimaNews = 1 WHERE `newsletter`.`newsletter` = "mlIsimaNews";');
+            //$this->addSql('INSERT INTO `subscriber` (`user_id`, `newsletter_id`) SELECT `User`.`id`,`newsletter`.`id` FROM User INNER JOIN newsletter ON mlInformations = 1 WHERE `newsletter`.`newsletter` = "mlInformations";');
+            //$this->addSql('INSERT INTO `subscriber` (`user_id`, `newsletter_id`) SELECT `User`.`id`,`newsletter`.`id` FROM User INNER JOIN newsletter ON mlEmployment = 1 WHERE `newsletter`.`newsletter` = "mlEmployments";');
+            //$this->addSql('INSERT INTO `subscriber` (`user_id`, `newsletter_id`) SELECT `User`.`id`,`newsletter`.`id` FROM User INNER JOIN newsletter ON mlEvents = 1 WHERE `newsletter`.`newsletter` = "mlEvents";');
+            //$this->addSql('INSERT INTO `subscriber` (`user_id`, `newsletter_id`) SELECT `User`.`id`,`newsletter`.`id` FROM User INNER JOIN newsletter ON mlIsimaNews = 1 WHERE `newsletter`.`newsletter` = "mlIsimaNews";');
           }
 
 
